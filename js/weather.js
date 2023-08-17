5e9d95c40e5b950d12660fa598e3ab25035028de
Stasiu <lemsta2303@gmail.com>
2023-08-17 14:51:45 +0200

    17.07, 14:50

--- js/weather.js
+++ js/weather.js
@@ -1,78 +1,135 @@
 const apiKey = '98b3e8ec96b066ca74e2cdfdcad1a2fe';
-let city = 'Sępólno Krajeńskie';
+let city = 'Gliwice';
 const apiUrl = (city_local, api_key) => 'https://api.openweathermap.org/data/2.5/forecast?q=' + city_local + '&units=metric&appid=' + api_key;
-let actualUnit = 'C';
+let temperatureUnit = 'C';
+let windSpeedUnit = 'km/h';
 const loadingOverlay = document.querySelector('.loading-overlay');
 
 
-function sendAndFetchApiRequest(cityName, index, unit){
+function sendAndFetchApiRequest(cityName, index, tempUnit, windUnit){
   fetch(apiUrl(cityName,apiKey))
   .then(response => response.json())
   .then(data => {
     console.log(data); 
-    useApiData(data, index, unit);
+    useApiData(data, index, tempUnit, windUnit);
   })
   .catch(error => {
     console.error('There was a problem with the fetch operation:', error);
   });
 }
 
-function useApiData(data, index, unit){
-  setBackgroundAndIcon(data, index);
-  setMainInformation(data, index, unit);
-  loadingOverlay.classList.add('loading-overlay-nodisplay');
+function useApiData(data, index, temperatureUnit, windUnit){
+  const setBackgroundAndIconPromise = setBackgroundAndIcon(data, index);
+  const setMainInformationPromise = setMainInformation(data, index, temperatureUnit, windUnit);
+  const setDailyForecastPromise = setDailyForecast(data,index,temperatureUnit,windUnit);
+  Promise.all([setBackgroundAndIconPromise, setMainInformationPromise, setDailyForecastPromise])
+    .then(() => {
+      setTimeout(() => {
+        loadingOverlay.classList.add('loading-overlay-nodisplay');
+      }, 100); 
+    })
+    .catch(error => {
+      console.error('An error occurred:', error);
+      setTimeout(() => {
+        loadingOverlay.classList.add('loading-overlay-nodisplay');
+      }, 100); 
+    });
 }
 
-function setMainInformation(data, index, unit){
-  const headerInfoCityName = document.querySelector('.header__info__city__name');
-  const headerInfoWeatherDescription = document.querySelector('.header__info__weather-description');
-  const headerInfoTemperature = document.querySelector('.header__info__temperature');
-  const headerInfoWeekDay = document.querySelector('.header__info__day__week-day');
-  const headerInfoDay = document.querySelector('.header__info__day__date');
-  const headerInfoTime = document.querySelector('.header__info__day__time');
-  headerInfoCityName.innerText = data.city.name;
-  headerInfoWeatherDescription.innerText = data.list[index].weather[0].description.toUpperCase();
-  headerInfoTemperature.innerText = returnTempreatureValueInChoosenUnit(data.list[index].main.temp, unit);
-  headerInfoWeekDay.innerText = getDayOfWeekWithTimezone(data.list[index].dt_txt, data.city.timezone);
-  headerInfoDay.innerText = formatDateWithTimezone(data.list[index].dt_txt, data.city.timezone);
-  headerInfoTime.innerText = extractTimeWithTimezone(data.list[index].dt_txt, data.city.timezone);
+function setMainInformation(data, index, temperatureUnit, windUnit){
+  return new Promise((resolve)=>{
+    const headerInfoCityName = document.querySelector('.header__info__city__name');
+    const headerInfoWeatherDescription = document.querySelector('.header__info__weather-description');
+    const headerInfoTemperature = document.querySelector('.header__info__temperature');
+    const headerInfoWeekDay = document.querySelector('.header__info__day__week-day');
+    const headerInfoDay = document.querySelector('.header__info__day__date');
+    const headerInfoTime = document.querySelector('.header__info__day__time');
+    const airConditionRealFeel = document.querySelector('.real-feel-value');
+    const airConditionWindSpeed = document.querySelector('.wind-speed-value');
+    const airConditionHumidity = document.querySelector('.humidity-value');
+    headerInfoCityName.innerText = data.city.name;
+    headerInfoWeatherDescription.innerText = data.list[index].weather[0].description.toUpperCase();
+    headerInfoTemperature.innerText = returnTempreatureValueInChoosenUnit(data.list[index].main.temp, temperatureUnit);
+    headerInfoWeekDay.innerText = getDayOfWeekWithTimezone(data.list[index].dt_txt, data.city.timezone);
+    headerInfoDay.innerText = formatDateWithTimezone(data.list[index].dt_txt, data.city.timezone);
+    headerInfoTime.innerText = extractTimeWithTimezone(data.list[index].dt_txt, data.city.timezone);
+    airConditionRealFeel.innerText = returnTempreatureValueInChoosenUnit(data.list[index].main.feels_like, temperatureUnit);
+    airConditionWindSpeed.innerText = returnWindSpeedInChoosenUnit(data.list[index].wind.speed,windUnit);
+    airConditionHumidity.innerText = data.list[index].main.humidity + '%';
+    resolve();
+    
+  });
 }
 
 function setBackgroundAndIcon(data, index){
-  const backgroundImg = document.querySelector('.bg-photo');
-  const bigIconImgMobile = document.querySelector('.big-icon-photo-mobile');
-  const bigIconImgDesktop = document.querySelector('.big-icon-photo-desktop');
-  const weatherType = data.list[index].weather[0].main;
-  // const weatherType = "Snow";
-  if (weatherType === "Clear"){
-    backgroundImg.src = '/img/background-images/clear.jpg';
-    backgroundImg.alt = "clear;"
-    bigIconImgMobile.src = '/img/weather-icon-images/clear.svg';
-    bigIconImgMobile.alt = "clear";
-    bigIconImgDesktop.src  = '/img/weather-icon-images/clear.svg';
-    bigIconImgDesktop.alt = "clear";
-  } else if (weatherType === "Clouds" ){
-    backgroundImg.src = '/img/background-images/clouds.jpg';
-    backgroundImg.alt = "clouds";
-    bigIconImgMobile.src = '/img/weather-icon-images/clouds.svg';
-    bigIconImgMobile.alt = "clouds";
-    bigIconImgDesktop.src  = '/img/weather-icon-images/clouds.svg';
-    bigIconImgDesktop.alt = "clouds";
-  } else if (weatherType === "Rain" ){
-    backgroundImg.src = '/img/background-images/rain.jpg';
-    backgroundImg.alt = "rain";
-    bigIconImgMobile.src = '/img/weather-icon-images/rain.svg';
-    bigIconImgMobile.alt = "rain";
-    bigIconImgDesktop.src  = '/img/weather-icon-images/rain.svg';
-    bigIconImgDesktop.alt = "rain";
-  } else if (weatherType === "Snow" ) {
-    backgroundImg.src = '/img/background-images/snow.jpg';
-    backgroundImg.alt = "snow";
-    bigIconImgMobile.src = '/img/weather-icon-images/snow.svg';
-    bigIconImgMobile.alt = "snow";
-    bigIconImgDesktop.src  = '/img/weather-icon-images/snow.svg';
-    bigIconImgDesktop.alt = "snow";
-  }
+  return new Promise((resolve) => {
+    const backgroundImg = document.querySelector('.bg-photo');
+    const bigIconImgMobile = document.querySelector('.big-icon-photo-mobile');
+    const bigIconImgDesktop = document.querySelector('.big-icon-photo-desktop');
+    const weatherType = data.list[index].weather[0].main;
+    if (weatherType === "Clear"){
+      backgroundImg.src = '/img/background-images/clear.webp';
+      backgroundImg.alt = "clear;"
+      bigIconImgMobile.src = '/img/weather-icon-images/clear.svg';
+      bigIconImgMobile.alt = "clear";
+      bigIconImgDesktop.src  = '/img/weather-icon-images/clear.svg';
+      bigIconImgDesktop.alt = "clear";
+    } else if (weatherType === "Clouds" ){
+      backgroundImg.src = '/img/background-images/clouds.webp';
+      backgroundImg.alt = "clouds";
+      bigIconImgMobile.src = '/img/weather-icon-images/clouds.svg';
+      bigIconImgMobile.alt = "clouds";
+      bigIconImgDesktop.src  = '/img/weather-icon-images/clouds.svg';
+      bigIconImgDesktop.alt = "clouds";
+    } else if (weatherType === "Rain" ){
+      backgroundImg.src = '/img/background-images/rain.webp';
+      backgroundImg.alt = "rain";
+      bigIconImgMobile.src = '/img/weather-icon-images/rain.svg';
+      bigIconImgMobile.alt = "rain";
+      bigIconImgDesktop.src  = '/img/weather-icon-images/rain.svg';
+      bigIconImgDesktop.alt = "rain";
+    } else if (weatherType === "Snow" ) {
+      backgroundImg.src = '/img/background-images/snow.webp';
+      backgroundImg.alt = "snow";
+      bigIconImgMobile.src = '/img/weather-icon-images/snow.svg';
+      bigIconImgMobile.alt = "snow";
+      bigIconImgDesktop.src  = '/img/weather-icon-images/snow.svg';
+      bigIconImgDesktop.alt = "snow";
+    }
+    resolve();
+  });
+}
+
+function setDailyForecast(data,index,temperatureUnit, windUnit){
+  return new Promise((resolve)=>{
+    const dailyForecastElements = document.querySelectorAll('.main__daily-forecast__graph__element');
+    const dailyForecastTemperatures = document.querySelectorAll('.main__daily-forecast__graph__element__temperature');
+    const dailyForecastIcons = document.querySelectorAll('.daily-forecast-element-icon');
+    const dailyForecastWindSpeed = document.querySelectorAll('.main__daily-forecast__graph__element__icon__wind');
+    const dailyForecastTime = document.querySelectorAll('.main__daily-forecast__graph__element__icon__time');
+    for( let i = 0; i<dailyForecastElements.length; i++ ) {
+      dailyForecastTemperatures[i].innerText = returnTempreatureValueInChoosenUnit(data.list[i].main.temp, temperatureUnit);
+      let weatherType = data.list[i].weather[0].main;
+      if (weatherType === "Clear"){
+        dailyForecastIcons[i].src = '/img/weather-icon-images/clear.svg';
+        dailyForecastIcons[i].alt = 'clear';
+      } else if (weatherType === "Clouds" ){
+        dailyForecastIcons[i].src = '/img/weather-icon-images/clouds.svg';
+        dailyForecastIcons[i].alt = 'clouds';
+      } else if (weatherType === "Rain" ){
+        dailyForecastIcons[i].src = '/img/weather-icon-images/rain.svg';
+        dailyForecastIcons[i].alt = 'rain';
+      } else if (weatherType === "Snow" ) {
+        dailyForecastIcons[i].src = '/img/weather-icon-images/snow.svg';
+        dailyForecastIcons[i].alt = 'snow';
+      }
+      dailyForecastWindSpeed[i].innerText = returnWindSpeedInChoosenUnit(data.list[i].wind.speed,windUnit);
+      if(i===0) dailyForecastTime[i].innerText = "Now";
+      else dailyForecastTime[i].innerText = extractTimeWithTimezone(data.list[i].dt_txt, data.city.timezone);
+    }
+    resolve();
+  });
+
 }
 
 function returnTempreatureValueInChoosenUnit(value, unit){
@@ -81,26 +138,11 @@ function returnTempreatureValueInChoosenUnit(value, unit){
   else if (unit == 'K') return Math.round( value + 273.15 ) + 'K';
 }
 
-// function getDayOfWeek(dateStr) {
-//   const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-//   const dateObject = new Date(dateStr);
-//   const dayIndex = dateObject.getDay();
-//   return daysOfWeek[dayIndex];
-// }
-
-// function formatDate(dateStr) {
-//   const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-//   const dateObject = new Date(dateStr);
-//   const formattedDate = `${dateObject.getDate()} ${months[dateObject.getMonth()]} ${dateObject.getFullYear()}`;
-//   return formattedDate;
-// }
-
-// function extractTime(dateStr) {
-//   const dateObject = new Date(dateStr);
-//   const hours = String(dateObject.getHours()).padStart(2, '0');
-//   const minutes = String(dateObject.getMinutes()).padStart(2, '0');
-//   return `${hours}:${minutes}`;
-// }
+function returnWindSpeedInChoosenUnit(value, unit){
+  if(unit==='m/s') return (value).toFixed(1) + 'm/s';
+  else if (unit==='km/h') return (value * 3.6).toFixed(1) + 'km/h';
+  else if (unit==='M/h') return (value * 2.2).toFixed(1) + 'M/h';
+ }
 
 function getDayOfWeekWithTimezone(dateStr, timezoneOffset) {
   const daysOfWeek = ['Niedziela', 'Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota'];
@@ -136,4 +178,4 @@ function extractTimeWithTimezone(dateStr, timezoneOffset) {
 }
 
 
-sendAndFetchApiRequest(city, 0, actualUnit);
+sendAndFetchApiRequest(city, 0, temperatureUnit, windSpeedUnit);
